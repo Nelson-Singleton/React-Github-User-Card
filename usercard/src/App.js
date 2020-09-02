c49a7f61e84273b29f428eb64026e2028606c3c4
nelson-singleton <nsingleton@ymail.com>
2020-09-01 19:12:42 -0500

    All components are functional. Styling remains.

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios'
 import './App.css';
 import Card from './Card'
+import Followers from './FollowerContainer'
 
 class App extends React.Component{
   constructor(){
@@ -20,13 +21,26 @@ componentDidMount(){
         myCard: res.data
         
       }))      
-    .catch (err => console.log("Error"))
+    .catch (err => console.log("Error fetching usercard"))
+
+
+  axios
+      .get ('https://api.github.com/users/Nelson-Singleton/followers')
+      .then (res =>
+        this.setState({
+          followers: res.data
+        }))
+    .catch (err => console.log("Error fetching followers"))
+
 }
+    
 
   render(){
   return (
     <div className="App">
      <Card mycard = {this.state.myCard} />
+     <h3>My fellow students:</h3>
+     <Followers followers = {this.state.followers}/>
     </div>
   );
 }
--- usercard/src/FollowerContainer.js
+++ usercard/src/FollowerContainer.js
@@ -1,8 +1,19 @@
-import react from 'react'
+import React from 'react'
+
 
 function Followers(props){
     return(
-            <div></div>
+            <div class = "followerContainer">
+                
+                    {
+                    props.followers.map(item =>
+                        <div class ="followerCard">
+                        <h3>{item.login}</h3>
+                        </div>     
+                    )                        
+                    }
+                
+            </div>
     )
 }
 
